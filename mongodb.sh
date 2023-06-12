c08a759bba2ef6a008077e6d74850f1c6d8f1bda
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-06-12 21:36:17 +0530

    New change

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,15 @@
-
+echo "Copying the mongodb repo file"
 cp /DevOps\ Training/GitHub-Repos/Roboshop-Shell/mongodb.repo  /etc/yum.repos.d/mongo.repo
 
+echo "Installing mongodb packages"
 yum install mongodb-org -y
 
+echo "Enabling and starting  mongod service"
 systemctl enable mongod
 systemctl start mongod
 
+echo "Editing the port"
 sed -e -i /127.0.0.1/0.0.0.0/ /etc/mongod.conf
 
+echo "restarting mongod service"
 systemctl restart mongod
