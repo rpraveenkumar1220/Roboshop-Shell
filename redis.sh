05f878c2573c51f22871891f598d33989a4200b9
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-07-08 14:47:56 +0530

    Basic Shell structure

--- redis.sh
+++ redis.sh
@@ -7,8 +7,10 @@ yum module enable redis:remi-6.2 -y
 echo "Installing redis package"
 yum install redis -y
 
-echo "Changing port"
+echo "Changing ports"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+
 
 echo "Enabling and startindg redis service"
 systemctl enable redis
