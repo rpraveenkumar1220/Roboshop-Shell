7e654e99808e824f8caa85c153bb76c18aa8c3e4
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-07-19 10:05:16 +0530

    Final Shell structure

--- mysql.sh
+++ mysql.sh
@@ -1,22 +1,28 @@
 source common.sh
 
 echo -e "${color} Disabling the current ${component} module ${nocolor}"
-yum module disable mysql -y   &>> /tmp/roboshop.log
+yum module disable mysql -y &>> ${log_file}
+stat_check $?
 
 echo -e "${color} copying mysql repo file ${nocolor}"
-cp /home/centos/Roboshop-Shell/mysql.repo /etc/yum.repos.d/mysql.repo  &>> /tmp/roboshop.log
+cp /home/centos/Roboshop-Shell/mysql.repo /etc/yum.repos.d/mysql.repo  &>> ${log_file}
+stat_check $?
 
 echo -e "${color} Installing mysql package ${nocolor}"
-yum install mysql-community-server -y   &>> /tmp/roboshop.log
+yum install mysql-community-server -y   &>> ${log_file}
+stat_check $?
 
 echo -e "${color} Enabling and starting mysqld service ${nocolor}"
-systemctl enable mysqld  &>> /tmp/roboshop.log
-systemctl start mysqld   &>> /tmp/roboshop.log
+systemctl enable mysqld  &>> ${log_file}
+systemctl start mysqld   &>> ${log_file}
+stat_check $?
 
 echo -e "${color} Setting password for mysql ${nocolor}"
-mysql_secure_installation --set-root-pass  $1  &>> /tmp/roboshop.log
+mysql_secure_installation --set-root-pass  $1  &>> ${log_file}
+stat_check $?
 
 echo -e "${color} Connecting to DB to check the password Authentication ${nocolor}"
-mysql -uroot -pRoboShop@1   &>> /tmp/roboshop.log
+mysql -uroot -pRoboShop@1   &>> ${log_file}
+stat_check $?
 
 
