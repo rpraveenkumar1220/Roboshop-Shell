ada900c1d8d3d044410d12e21661c3c40e87910f
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-07-19 09:48:03 +0530

    Final Shell structure

--- redis.sh
+++ redis.sh
@@ -2,18 +2,21 @@ source common.sh
 
 echo -e "${color} Installing redis rpm ${nocolor}"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y  &>> ${log_file}
+stat_check $?
 
 echo -e "${color} Enabling redis module ${nocolor}"
 yum module enable redis:remi-6.2 -y  &>> ${log_file}
+stat_check $?
 
 echo -e "${color} Installing redis package ${nocolor}"
 yum install redis -y   &>> ${log_file}
+stat_check $?
 
 echo -e "${color} Changing ports ${nocolor}"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf  &>> ${log_file}
-
+stat_check $?
 
 echo -e "${color} Enabling and starting redis service ${nocolor}"
 systemctl enable redis  &>> ${log_file}
 systemctl start redis   &>> ${log_file}
-
+stat_check $?
