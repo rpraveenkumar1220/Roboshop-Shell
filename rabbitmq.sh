d69550368a0e810a85eb151bbbe749bf2bed2120
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-07-19 11:35:54 +0530

    Final Shell structure

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,17 +2,22 @@ source common.sh
 
 echo -e "${color} Downloading erlang repositories ${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash   &>> ${log_file}
+stat_check $?
 
 echo -e "${color} Downloading rabbitmq repositories ${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>> ${log_file}
+stat_check $?
 
 echo -e "${color} Installing rabbitmq packages ${nocolor}"
 yum install rabbitmq-server -y   &>> ${log_file}
+stat_check $?
 
 echo -e "${color} Enabling and starting rabbitmq server ${nocolor}"
 systemctl enable rabbitmq-server  &>> ${log_file}
 systemctl start rabbitmq-server   &>> ${log_file}
+stat_check $?
 
 echo -e "${color} Adding app user and setting permissions ${nocolor}"
 rabbitmqctl add_user roboshop  $1   &>> ${log_file}
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"   &>> ${log_file}
+stat_check $?
